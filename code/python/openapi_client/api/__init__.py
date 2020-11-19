a9d2e8f2afeb6a41cc59e860e607a5b1ef627db7
MelvLee <MelvLee@users.noreply.github.com>
2020-11-19 08:46:45 +0000

    commit generated sdk artifacts

---
+++ code/python/openapi_client/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.beheren_volgindicaties_api import BeherenVolgindicatiesApi
+from openapi_client.api.raadplegen_gewijzigde_personen_api import RaadplegenGewijzigdePersonenApi
